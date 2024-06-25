f5e67522ffe160841877a9eb07d54c39c8cb34c7
Zilin Zhu <zilinzhu@tencent.com>
2024-06-25 19:25:51 +0800

    make the repo a bit more formal

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aqt",
+    version="0.1",
+    author="zhuzilin",
+    url="https://github.com/zhuzilin/aqt-pytorch",
+    packages=find_packages(),
+)
